910f4be150294f759691fab297d4ebc1afacf4e9
Dergf3 <70668420+Dergf3@users.noreply.github.com>
2022-10-14 19:22:06 -0500

    Create fizzbuzz1.py
    First iteration

---
+++ fizzbuzz1.py
@@ -0,0 +1,13 @@
+#Getting to run at least until 25
+
+for fizzbuzz in range(25):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)

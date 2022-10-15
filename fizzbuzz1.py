f70460a12b6be0ccf600c64de5bff036487d25ac
Dergf3 <70668420+Dergf3@users.noreply.github.com>
2022-10-14 19:27:50 -0500

    Update fizzbuzz1.py

--- fizzbuzz1.py
+++ fizzbuzz1.py
@@ -1,13 +1,14 @@
 #Getting to run at least until 25
 
-for fizzbuzz in range(25):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+for i in range(1,100):
+    if i % 3 == 0 and i % 5 == 0:
         print("fizzbuzz")
         continue
-    elif fizzbuzz % 3 == 0:
+    elif i % 3 == 0:
         print("fizz")
         continue
-    elif fizzbuzz % 5 == 0:
+    elif i % 5 == 0:
         print("buzz")
         continue
-    print(fizzbuzz)
+    else:
+        print(i)
